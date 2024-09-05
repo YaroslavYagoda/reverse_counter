359a0bcc5126cbb397101dd8b8a465797dc08bb6
Ярослав <Yagavdv14@yandex.ru>
2024-09-05 12:49:49 +0300

    fixed it

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 schet = input('Введите положительное целое число: ')
 try:
     schet = int(schet)
-except Exception:
+except ValueError:
     print('Неверный формат числа!')
     exit()
+
 while schet >= 0:
     print(schet)
     schet -= 1
